5234abd38bda237f8804c0c40c0b6cbf588a9e1a
masyagin1998 <masyagin1998@yandex.ru>
2018-07-22 15:43:45 +0300

    Add new data type

---
+++ libtelco/sessions/data-types/resourses.go
@@ -0,0 +1,29 @@
+package dataTypes
+
+/*
+Ресурсы.
+*/
+
+// SchoolResources struct содержит в себе школьные ресурсы.
+type SchoolResources struct {
+	Data []Group `json:"data"`
+}
+
+// File struct struct содержит в себе один файл в школьных ресурсах.
+type File struct {
+	Name string `json:"name"`
+	Link string `json:"link"`
+}
+
+// Subgroup struct содержит в себе подгруппы файлов.
+type Subgroup struct {
+	SubgroupTitle string `json:"subgroupTitle"`
+	Files         []File `json:"files"`
+}
+
+// Group struct содержит в себе одну группу файлов в школьных ресурсах.
+type Group struct {
+	GroupTitle string     `json:"groupTitle"`
+	Files      []File     `json:"files"`
+	Subgroups  []Subgroup `json:"subgroups"`
+}
---
+++ libtelco/sessions/resourses.go
@@ -0,0 +1,7 @@
+// Copyright (C) 2018 Mikhail Masyagin
+
+package sessions
+
+/*
+Package sessions - данный файл содержит в себе получение ресурсов.
+*/
