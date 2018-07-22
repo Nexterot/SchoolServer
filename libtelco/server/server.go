52459040db0878d9e9015459445504ce97b13b8a
masyagin1998 <masyagin1998@yandex.ru>
2018-07-22 15:23:23 +0300

    За себя и за Сашку

--- libtelco/server/server.go
+++ libtelco/server/server.go
@@ -9,10 +9,7 @@ import (
 	cp "SchoolServer/libtelco/config-parser"
 	"SchoolServer/libtelco/log"
 	api "SchoolServer/libtelco/rest-api"
-	"fmt"
-	"os"
-
-	ss "SchoolServer/libtelco/sessions"
+	"net/http"
 
 	"runtime"
 )
@@ -37,24 +34,7 @@ func NewServer(config *cp.Config, logger *log.Logger) *Server {
 func (serv *Server) Run() error {
 	// Задаем максимальное количество потоков.
 	runtime.GOMAXPROCS(serv.config.MaxProcs)
-
-	// TODO: протестировать все Get'ы.
-
-	s := ss.NewSession(&serv.config.Schools[0])
-	err := s.Login()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	err = s.Logout()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println("Rock")
-	return nil
-
 	// Подключаем handler'ы из RestAPI.
-	//serv.api.BindHandlers()
-	//return http.ListenAndServe(serv.config.ServerAddr, nil)
+	serv.api.BindHandlers()
+	return http.ListenAndServe(serv.config.ServerAddr, nil)
 }
