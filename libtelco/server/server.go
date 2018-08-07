3f10a14722098a5a194f8fe1dbf97a9522e35ae8
masyagin1998 <masyagin1998@yandex.ru>
2018-08-07 22:01:46 +0300

    Little change

--- libtelco/server/server.go
+++ libtelco/server/server.go
@@ -8,10 +8,8 @@ package server
 import (
 	cp "SchoolServer/libtelco/config-parser"
 	"SchoolServer/libtelco/log"
-	//"fmt"
-	//"time"
 
-	//ss "SchoolServer/libtelco/sessions"
+	// ss "SchoolServer/libtelco/sessions"
 
 	api "SchoolServer/libtelco/rest-api"
 
@@ -42,12 +40,53 @@ func (serv *Server) Run() error {
 	// Задаем максимальное количество потоков.
 	runtime.GOMAXPROCS(serv.config.MaxProcs)
 
-	// Закрыть БД
-	defer serv.api.Db.Close()
-	defer serv.api.Redis.Close()
-	// Закрыть кукистор
-	defer serv.api.Store.Close()
+	/*
+		// Тесты.
+		kek := ss.NewSession(&serv.config.Schools[0])
+		err := kek.Login()
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = kek.GetChildrenMap()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		err = kek.GetChildrenMap()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+			err = kek.GetChildrenMap()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	*/
 
 	serv.api.BindHandlers()
+	defer func() {
+		_ = serv.api.Db.Close()
+		_ = serv.api.Redis.Close()
+		_ = serv.api.Store.Close()
+	}()
 	return http.ListenAndServe(serv.config.ServerAddr, context.ClearHandler(http.DefaultServeMux))
 }
--- libtelco/sessions/type-01/check.go
+++ libtelco/sessions/type-01/check.go
@@ -13,9 +13,11 @@ import (
 	gr "github.com/levigross/grequests"
 )
 
-// checkResponse проверяет, не выкинуло ли нас с сервера.
+// checkResponse проверяет, не выкинуло ли нас с сервера и не пришло ли предупреждение.
 func checkResponse(s *ss.Session, response *gr.Response) error {
 	str := string(response.Bytes())
+	fmt.Println(str)
+	// Проверка на "выброс с сервера".
 	if (response.StatusCode != 200) || strings.Contains(strings.ToLower(str), "<title></title>") {
 		return fmt.Errorf("You was logged out from server")
 	}
