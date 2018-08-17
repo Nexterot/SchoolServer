37b62a34ca68760c5f5133f2b4ad647ec8fc032b
Alexander Barluka <nexterot@mail.ru>
2018-08-17 18:37:18 +0300

    Added mail handlers
    /get_mail
    /get_mail_description

--- libtelco/rest-api/rest-api.go
+++ libtelco/rest-api/rest-api.go
@@ -115,8 +115,8 @@ func (rest *RestAPI) BindHandlers() {
 	// Школьные ресурсы
 	http.HandleFunc("/get_resources", rest.Handler)
 	// Почта
-	http.HandleFunc("/get_mail", rest.Handler)
-	http.HandleFunc("/get_mail_description", rest.Handler)
+	http.HandleFunc("/get_mail", rest.GetMailHandler)                        // in dev
+	http.HandleFunc("/get_mail_description", rest.GetMailDescriptionHandler) // in dev
 	http.HandleFunc("/delete_mail", rest.Handler)
 	http.HandleFunc("/send_letter", rest.Handler)
 	http.HandleFunc("/get_address_book", rest.Handler)
@@ -2018,6 +2018,207 @@ func (rest *RestAPI) FileHandler(respwr http.ResponseWriter, req *http.Request) 
 		rest.logger.Error("REST: Error occured when sending file", "Error", err, "Filename", fileName, "Status", status, "IP", req.RemoteAddr)
 	} else {
 		rest.logger.Info("REST: Successfully sent response", "Filename", fileName, "IP", req.RemoteAddr)
+	}
+}
+
+// getMailRequest используется в GetMailHandler
+type getMailRequest struct {
+	Section    int    `json:"section"`
+	PageSize   int    `json:"pageSize"`
+	StartIndex int    `json:"startIndex"`
+	Order      string `json:"order"`
+}
+
+// GetMailHandler обрабатывает запросы на получение списка писем
+func (rest *RestAPI) GetMailHandler(respwr http.ResponseWriter, req *http.Request) {
+	rest.logger.Info("REST: GetMailHandler called", "IP", req.RemoteAddr)
+	// Проверка метода запроса
+	if req.Method != "POST" {
+		rest.logger.Info("REST: Wrong method", "Method", req.Method, "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	// Получить существующие имя и объект локальной сессии
+	sessionName, session := rest.getLocalSession(respwr, req)
+	if session == nil {
+		return
+	}
+	// Чтение запроса от клиента
+	var rReq getMailRequest
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&rReq)
+	if err != nil {
+		rest.logger.Info("REST: Malformed request data", "Error", err.Error(), "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusBadRequest)
+		resp := "malformed data"
+		status, err := respwr.Write([]byte(resp))
+		if err != nil {
+			rest.logger.Error("REST: Error occured when sending response", "Error", err, "Response", resp, "Status", status, "IP", req.RemoteAddr)
+		} else {
+			rest.logger.Info("REST: Successfully sent response", "Response", resp, "IP", req.RemoteAddr)
+		}
+		return
+	}
+	// Распечатаем запрос от клиента
+	rest.logger.Info("REST: Request data", "Data", rReq, "IP", req.RemoteAddr)
+	// Получим удаленную сессию
+	remoteSession, ok := rest.sessionsMap[sessionName]
+	if !ok {
+		// Если нет удаленной сессии
+		rest.logger.Info("REST: No remote session", "IP", req.RemoteAddr)
+		// Создать новую
+		remoteSession = rest.remoteLogin(respwr, req, session)
+		if remoteSession == nil {
+			return
+		}
+	}
+	// Если поле page пустое, page = 1
+	if rReq.Section == 0 {
+		rReq.Section = 1
+	}
+	// Проверим валидность данных
+	if rReq.Section < 1 || rReq.Section > 4 {
+		rest.logger.Info("REST: Invalid data", "Error", "must be between 1 and 4", "Section", rReq.Section, "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusBadRequest)
+		resp := "invalid page"
+		status, err := respwr.Write([]byte(resp))
+		if err != nil {
+			rest.logger.Error("REST: Error occured when sending response", "Error", err, "Response", resp, "Status", status, "IP", req.RemoteAddr)
+		} else {
+			rest.logger.Info("REST: Successfully sent response", "Response", resp, "IP", req.RemoteAddr)
+		}
+		return
+	}
+	// Сходить за списком писем по удаленной сесиии
+	emailsList, err := remoteSession.GetEmailsList(strconv.Itoa(rReq.Section), strconv.Itoa(rReq.StartIndex), strconv.Itoa(rReq.PageSize), rReq.Order)
+	if err != nil {
+		if err.Error() == "You was logged out from server" {
+			// Если удаленная сессия есть, но не активна
+			rest.logger.Info("REST: Remote connection timed out", "IP", req.RemoteAddr)
+			// Создать новую
+			remoteSession = rest.remoteLogin(respwr, req, session)
+			if remoteSession == nil {
+				return
+			}
+			// Повторно получить с сайта школы
+			emailsList, err = remoteSession.GetEmailsList(strconv.Itoa(rReq.Section), strconv.Itoa(rReq.StartIndex), strconv.Itoa(rReq.PageSize), rReq.Order)
+			if err != nil {
+				// Ошибка
+				rest.logger.Error("REST: Error occured when getting data from site", "Error", err, "IP", req.RemoteAddr)
+				respwr.WriteHeader(http.StatusBadGateway)
+				return
+			}
+		} else {
+			// Другая ошибка
+			rest.logger.Error("REST: Error occured when getting data from site", "Error", err, "IP", req.RemoteAddr)
+			respwr.WriteHeader(http.StatusBadGateway)
+			return
+		}
+	}
+	// Закодировать ответ в JSON
+	bytes, err := json.Marshal(emailsList)
+	if err != nil {
+		rest.logger.Error("REST: Error occured when marshalling response", "Error", err, "Response", emailsList, "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Отправить ответ клиенту
+	status, err := respwr.Write(bytes)
+	if err != nil {
+		rest.logger.Error("REST: Error occured when sending response", "Error", err, "Response", emailsList, "Status", status, "IP", req.RemoteAddr)
+	} else {
+		rest.logger.Info("REST: Successfully sent response", "Response", emailsList, "IP", req.RemoteAddr)
+	}
+}
+
+// getMailDescriptionRequest используется в GetMailDescriptionHandler
+type getMailDescriptionRequest struct {
+	MID  string `json:"mid"`
+	MBID string `json:"mbid"`
+}
+
+// GetMailDescriptionHandler обрабатывает запросы на получение подробностей письма
+func (rest *RestAPI) GetMailDescriptionHandler(respwr http.ResponseWriter, req *http.Request) {
+	rest.logger.Info("REST: GetMailDescriptionHandler called", "IP", req.RemoteAddr)
+	// Проверка метода запроса
+	if req.Method != "POST" {
+		rest.logger.Info("REST: Wrong method", "Method", req.Method, "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	// Получить существующие имя и объект локальной сессии
+	sessionName, session := rest.getLocalSession(respwr, req)
+	if session == nil {
+		return
+	}
+	// Чтение запроса от клиента
+	var rReq getMailDescriptionRequest
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&rReq)
+	if err != nil {
+		rest.logger.Info("REST: Malformed request data", "Error", err.Error(), "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusBadRequest)
+		resp := "malformed data"
+		status, err := respwr.Write([]byte(resp))
+		if err != nil {
+			rest.logger.Error("REST: Error occured when sending response", "Error", err, "Response", resp, "Status", status, "IP", req.RemoteAddr)
+		} else {
+			rest.logger.Info("REST: Successfully sent response", "Response", resp, "IP", req.RemoteAddr)
+		}
+		return
+	}
+	// Распечатаем запрос от клиента
+	rest.logger.Info("REST: Request data", "Data", rReq, "IP", req.RemoteAddr)
+	// Получим удаленную сессию
+	remoteSession, ok := rest.sessionsMap[sessionName]
+	if !ok {
+		// Если нет удаленной сессии
+		rest.logger.Info("REST: No remote session", "IP", req.RemoteAddr)
+		// Создать новую
+		remoteSession = rest.remoteLogin(respwr, req, session)
+		if remoteSession == nil {
+			return
+		}
+	}
+	// Сходить по удаленной сесиии
+	emailDesc, err := remoteSession.GetEmailDescription(rReq.MID, rReq.MBID)
+	if err != nil {
+		if err.Error() == "You was logged out from server" {
+			// Если удаленная сессия есть, но не активна
+			rest.logger.Info("REST: Remote connection timed out", "IP", req.RemoteAddr)
+			// Создать новую
+			remoteSession = rest.remoteLogin(respwr, req, session)
+			if remoteSession == nil {
+				return
+			}
+			// Повторно получить с сайта школы
+			emailDesc, err = remoteSession.GetEmailDescription(rReq.MID, rReq.MBID)
+			if err != nil {
+				// Ошибка
+				rest.logger.Error("REST: Error occured when getting data from site", "Error", err, "IP", req.RemoteAddr)
+				respwr.WriteHeader(http.StatusBadGateway)
+				return
+			}
+		} else {
+			// Другая ошибка
+			rest.logger.Error("REST: Error occured when getting data from site", "Error", err, "IP", req.RemoteAddr)
+			respwr.WriteHeader(http.StatusBadGateway)
+			return
+		}
+	}
+	// Закодировать ответ в JSON
+	bytes, err := json.Marshal(emailDesc)
+	if err != nil {
+		rest.logger.Error("REST: Error occured when marshalling response", "Error", err, "Response", emailDesc, "IP", req.RemoteAddr)
+		respwr.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Отправить ответ клиенту
+	status, err := respwr.Write(bytes)
+	if err != nil {
+		rest.logger.Error("REST: Error occured when sending response", "Error", err, "Response", emailDesc, "Status", status, "IP", req.RemoteAddr)
+	} else {
+		rest.logger.Info("REST: Successfully sent response", "Response", emailDesc, "IP", req.RemoteAddr)
 	}
 }
 
