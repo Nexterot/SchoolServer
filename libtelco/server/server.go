3061b7244ff1b8a69583679861be98d015c5b21c
masyagin1998 <masyagin1998@yandex.ru>
2018-08-23 23:19:11 +0300

    Add network for profile

--- libtelco/server/server.go
+++ libtelco/server/server.go
@@ -6,8 +6,9 @@ Package server содержит основную функциональност�
 package server
 
 import (
+	"fmt"
 
-	// ss "github.com/masyagin1998/SchoolServer/libtelco/sessions"
+	ss "github.com/masyagin1998/SchoolServer/libtelco/sessions"
 
 	cp "github.com/masyagin1998/SchoolServer/libtelco/config-parser"
 	"github.com/masyagin1998/SchoolServer/libtelco/log"
@@ -41,22 +42,22 @@ func (serv *Server) Run() error {
 	// Задаем максимальное количество потоков.
 	runtime.GOMAXPROCS(serv.config.MaxProcs)
 
-	/*
-		// ТЕСТЫ.
-		kek := ss.NewSession(&serv.config.Schools[0])
-		err := kek.Login()
-		if err != nil {
-			fmt.Println(err)
-		}
+	// ТЕСТЫ.
+	kek := ss.NewSession(&serv.config.Schools[0])
+	err := kek.Login()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-		if err := kek.CreateEmail("11200", "11200", "11200", "11200", "JUST ANOTHER TEST", ")0)))"); err != nil {
-			fmt.Println(err)
-		}
+	data, err := kek.GetProfile()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
 
-		if err := kek.Logout(); err != nil {
-			fmt.Println(err)
-		}
-	*/
+	if err := kek.Logout(); err != nil {
+		fmt.Println(err)
+	}
 
 	serv.api.BindHandlers()
 	defer func() {
---
+++ libtelco/sessions/servers/01/profile/profile.go
@@ -0,0 +1,69 @@
+// Copyright (C) 2018 Mikhail Masyagin & Andrey Koshelev
+
+package profile
+
+import (
+	"fmt"
+
+	gr "github.com/levigross/grequests"
+	dt "github.com/masyagin1998/SchoolServer/libtelco/sessions/datatypes"
+	"github.com/masyagin1998/SchoolServer/libtelco/sessions/servers/01/check"
+	"github.com/pkg/errors"
+)
+
+// GetProfile получает подробности профиля с сервера первого типа.
+func GetProfile(s *dt.Session) (*dt.Profile, error) {
+	p := "http://"
+
+	// 0-ой Post-запрос.
+	r0 := func() ([]byte, bool, error) {
+		ro := &gr.RequestOptions{
+			Data: map[string]string{
+				"UID": "",
+				"VER": s.VER,
+				"at":  s.AT,
+			},
+			Headers: map[string]string{
+				"Origin":                    p + s.Serv.Link,
+				"Upgrade-Insecure-Requests": "1",
+				"Referer":                   p + s.Serv.Link,
+			},
+		}
+		r, err := s.Sess.Post(p+s.Serv.Link+"/asp/MySettings/MySettings.asp", ro)
+		if err != nil {
+			return nil, false, err
+		}
+		defer func() {
+			_ = r.Close()
+		}()
+		flag, err := check.CheckResponse(s, r)
+		if err != nil {
+			return nil, false, err
+		}
+		return r.Bytes(), flag, nil
+	}
+	b, flag, err := r0()
+	if err != nil {
+		return nil, errors.Wrap(err, "0 POST")
+	}
+	if !flag {
+		b, flag, err = r0()
+		if err != nil {
+			return nil, errors.Wrap(err, "retrying 0 POST")
+		}
+		if !flag {
+			return nil, fmt.Errorf("retry didn't work for 0 POST")
+		}
+	}
+	// Если мы дошли до этого места, то можно распарсить HTML-страницу,
+	// находящуюся в теле ответа, и найти в ней мапу детей в их ID.
+	/*
+		parsedHTML, err := html.Parse(bytes.NewReader(b))
+		if err != nil {
+			return nil, errors.Wrap(err, "parsing HTML")
+		}
+	*/
+	fmt.Println(string(b))
+
+	return nil, nil
+}
