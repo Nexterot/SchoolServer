6defde18574033adae59b07d5d35a7647048593d
Alexander Barluka <nexterot@mail.ru>
2018-12-08 21:17:40 +0300

    Added more logging

--- libtelco/sql-db/diary.go
+++ libtelco/sql-db/diary.go
@@ -125,7 +125,7 @@ func (db *Database) TaskMarkSeen(userName string, schoolID int, AID, CID, TP int
 		// Получим день по DayID
 		err = db.SchoolServerDB.First(&day, t.DayID).Error
 		if err != nil {
-			return errors.Wrapf(err, "Error query day with id='%v'", t.DayID)
+			return errors.Wrapf(err, "Error query day with id='%v', task='%v'", t.DayID, t)
 		}
 		// Получим студента по дню
 		err = db.SchoolServerDB.First(&student, day.StudentID).Error
